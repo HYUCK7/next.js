ba7e221b0e4c51d30a7ff2b1817ff939b73527ed
HYUCK7 <iqeq12191219@gmail.com>
2022-04-18 23:25:13 +0900

    2022-04-18 23:25

--- pages/common/Nav.js
+++ pages/common/Nav.js
@@ -12,16 +12,36 @@ export default function Nav(){
         <li className={styles.li}> <Link href='/basic/bmi'>BMI</Link> </li>
         <li className={styles.li}> <Link href='/todo/todolist'>스케줄목록</Link></li>
         <li className={styles.li}> <Link href='/todo/todo'>스케줄등록</Link></li>
+
+        <li className={styles.li}> <Link href='/todo/addTodo'>Todo추가</Link></li>
+        <li className={styles.li}> <Link href='/todo/modifyTodo'>Todo수정</Link></li>
+        <li className={styles.li}> <Link href='/todo/getTodos'>Todo목록</Link></li>
+        <li className={styles.li}> <Link href='/todo/removeTodo'>Todo삭제</Link></li>
+
         <li className={styles.li}> <Link href='/board/board-list/'>Board</Link></li>
+        <li className={styles.li}> <Link href='/board/addArticle/'>게시글등록</Link></li>
+        <li className={styles.li}> <Link href='/board/getArticles/'>게시글리스트</Link></li>
+        <li className={styles.li}> <Link href='/board/updateArticle/'>게시글수정</Link></li>
+        <li className={styles.li}> <Link href='/board/removeArticle/'>게시글삭제</Link></li>
+
         <li className={styles.li}> <Link href='/game/game-list'>game</Link></li>
+        <li className={styles.li}> <Link href='/game/addGame'>게임추가</Link></li>
+        <li className={styles.li}> <Link href='/game/getGames'>게임리스트</Link></li>
+        <li className={styles.li}> <Link href='/game/removeGame'>게임삭제</Link></li>
+        <li className={styles.li}> <Link href='/game/updateGame'>게임수정</Link></li>
+
         <li className={styles.li}> <Link href='/game/team-list/'>team</Link></li>
         <li className={styles.li}> <Link href='/user/join/'>Join</Link></li>
         <li className={styles.li}> <Link href='/user/login/'>login</Link></li>
+        <li className={styles.li}> <Link href='/user/logout/'>logout</Link></li>
         <li className={styles.li}> <Link href='/admin/dashboard/'>대시보드</Link></li>
         <li className={styles.li}> <Link href='/board/board-form/'>보드폼</Link></li>
-        <li className={styles.li}> <Link href='/user/userlist/'>유저 리스트</Link></li>
+        <li className={styles.li}> <Link href='/user/getUsers/'>유저 리스트</Link></li>
         <li className={styles.li}> <Link href='/board/boardlist/'>선수 리스트</Link></li>
         <li className={styles.li}> <Link href='/user/Profile/'>사용자 프로필</Link></li>
+        <li className={styles.li}> <Link href='/user/updateUser/'>사용자 수정</Link></li>
+        <li className={styles.li}> <Link href='/user/withdrawUser/'>회원탈퇴</Link></li>
+
       </ul>
     </nav>
   )
---
+++ pages/todo/addTodo.js
@@ -0,0 +1,61 @@
+import { table } from "console";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import tableStyle from '../common/styles/tableStyle.module.css'
+
+export default function AddTodo(){
+    const [todo, setTodo] = useState({userid: '', task: '', completed: ''})
+    const dispatch = useDispatch()
+
+    const handleChange = e => {
+        e.preventDefault()
+        const {name, value} = e.target;
+        setTodo({...todo, [name]: value})
+    }
+    return(
+        <div className = "todoapp stack-large">
+        <form onSubmit={ e => {
+            e.preventDefault
+            if(inputs) dispatch(addTodo(inputs))
+        }}>
+        <table className={tableStyle.table}>
+            <thead>
+                <tr>
+                    <th><h1>일정추가</h1></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <label>작성자</label>
+                    </td>
+                    <td>
+                    <input type='text' onChange={handleChange} name = 'userid' placeholder="사용자 아이디 입력"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                    <label htmlFor="task">할일</label>
+                    </td>
+                    <td>
+                        <input type='textarea' id='task' name="task" onChange={handleChange} style={{height:200 + "px"}}></input>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>Completed</label>
+                    </td>
+                    <td>
+                        <input type='text' onChange={handleChange} name = 'completed'/>
+                    </td>
+                </tr>
+                <tr>
+                    <td colSpan={3}><input type="submit" value="SUBMIT"/></td>
+                </tr>
+            </tbody>
+        </table>
+        </form>
+
+        </div>
+    )
+}
